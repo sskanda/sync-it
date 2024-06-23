be7b28de63c75f00514646e87d07a466c25aed6e
Skanda <sskanda98@gmail.com>
2024-06-23 18:25:05 +0530

    get all posts on home page

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -20,6 +20,23 @@ const createPost = async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 };
+
+const getPosts = async (req, res) => {
+  try {
+    let sortBy = "-createdAt";
+
+    let posts = await Post.find()
+      .populate("poster", "-password")
+      .sort(sortBy)
+      .lean();
+
+    return res.json(posts);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
+
 module.exports = {
   createPost,
+  getPosts,
 };
