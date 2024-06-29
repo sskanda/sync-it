3646f4082f8eda91cc3ab7b80ae05818afa972c2
Skanda <sskanda98@gmail.com>
2024-06-30 02:38:32 +0530

    add logout feature

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from "react";
 import { Stack, TextField, Button } from "@mui/material";
 import { Box } from "@mui/system";
 import { Link, useNavigate } from "react-router-dom";
+import { isLoggedIn, logoutUser } from "../helper/auth";
 import "./Navbar.css";
 const Navbar = () => {
+  const user = isLoggedIn();
   const navigate = useNavigate();
   const [search, setSearch] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     //navigate("/search?" + new URLSearchParams({ search }));
   };
-
+  const handleLogout = async (e) => {
+    logoutUser();
+    navigate("/login");
+  };
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
@@ -28,14 +33,25 @@ const Navbar = () => {
             value={search}
           />
         </Box>
-        <>
-          <Button variant="text" sx={{ minWidth: 80 }} href="/signup">
-            Sign Up
+        {user ? (
+          <Button
+            onClick={handleLogout}
+            variant="text"
+            sx={{ minWidth: 65 }}
+            href="/login"
+          >
+            LOGOUT
           </Button>
-          <Button variant="text" sx={{ minWidth: 65 }} href="/login">
-            Login
-          </Button>
-        </>
+        ) : (
+          <>
+            <Button variant="text" sx={{ minWidth: 80 }} href="/signup">
+              Sign Up
+            </Button>
+            <Button variant="text" sx={{ minWidth: 65 }} href="/login">
+              Login
+            </Button>
+          </>
+        )}
       </div>
     </Stack>
   );
